af68a9a46bfe44fa340f8f74a9754cab985763df
Sigve Hansen <sighan12@gmail.com>
2022-12-22 14:49:26 +0100

    feat(meals): add meals page

---
+++ src/routes/meals/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Meal } from 'src/types'
+	import { getContext } from 'svelte'
+	import MealListItem from '$lib/components/meal/MealListItem.svelte'
+	import MealList from '$lib/components/meal/MealList.svelte'
+
+	const meals = getContext<Meal[]>('meals')
+</script>
+
+<MealList>
+	{#each meals as meal (meal.id)}
+		<MealListItem {...meal} />
+	{/each}
+</MealList>
