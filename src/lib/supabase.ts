c3cefb370a8dc2706969880195018b3966ded0c6
Sigve Hansen <sighan12@gmail.com>
2022-12-22 13:42:22 +0100

    fix(env): use private env key for token

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from '$env/dynamic/public'
+import { env as publicEnv } from '$env/dynamic/public'
+import { env as privateEnv } from '$env/dynamic/private'
 
-const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+const supabase = createClient(publicEnv.PUBLIC_SUPABASE_URL, privateEnv.PRIVATE_SUPABASE_ANON_KEY)
 export default supabase
