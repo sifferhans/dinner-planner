f5c7f5a7052983222ba7e4a609756e49c219cf4f
Sigve Hansen <sighan12@gmail.com>
2022-12-22 17:06:35 +0100

    feat: add basic form elements

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let href: string | null = null
+	export let block: boolean = false
+</script>
+
+<svelte:element this={href ? 'a' : 'button'} {href} class="button" class:block {...$$restProps}>
+	<slot />
+</svelte:element>
+
+<style>
+	.button {
+		background: #f0f0f0;
+		border: none;
+		border-radius: 0.5rem;
+		padding: 0.5rem 1rem;
+		color: inherit;
+		font: inherit;
+		text-decoration: none;
+
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	.button.block {
+		display: flex;
+		width: 100%;
+	}
+</style>
---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let label: string
+</script>
+
+<label>
+	<span>{label}</span>
+	<input {...$$restProps} />
+</label>
+
+<style>
+	label {
+		display: grid;
+		gap: 0.5rem;
+	}
+
+	input {
+		padding: 0.5rem 1rem;
+		border: 1px solid;
+		border-radius: 0.5rem;
+	}
+
+	input::placeholder {
+		opacity: 0.7;
+	}
+
+	span {
+		display: flex;
+		justify-content: space-between;
+		gap: 0.5rem;
+	}
+
+	label:has(input:not(:required)) span::after {
+		content: '(opsjonelt)';
+		opacity: 0.5;
+	}
+</style>
